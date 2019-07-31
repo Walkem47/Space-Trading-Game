a1e62ebd24d96739201718bab1a8b4772c4175cc
Matthew.Walker <walkem47@my.erau.edu>
2019-07-31 06:15:48 -0700

    add the planet class

--- SpaceTradeGame/Planet.cs
+++ SpaceTradeGame/Planet.cs
@@ -1,6 +1,14 @@
-﻿namespace SpaceTradeGame
+﻿using System; 
+
+
+
+namespace SpaceTradeGame
 {
     public class Planet
     {
+        public string name { get; set; }
+        public Island[] Islands { get; set; }
+
+        public (double, double) location { get; set; }
     }
 }
