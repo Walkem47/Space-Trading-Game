71e3647762a72de2aa7601a217f16dee8eca1841
Matthew.Walker <walkem47@my.erau.edu>
2019-07-31 06:27:58 -0700

    add Island class

--- SpaceTradeGame/Island.cs
+++ SpaceTradeGame/Island.cs
@@ -6,5 +6,12 @@ namespace SpaceTradeGame
 {
     public class Island
     {
+        public string name { get; set; }
+        public (double, double) location { get; set; }
+
+        public IslandStatus islandStatus { get; set; }
+
     }
+
+    public enum IslandStatus { Hostile, Trading }; 
 }
