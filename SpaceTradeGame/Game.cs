bb4ce84f74e8ffe077fdd3bf7606efacebee39b7
Matthew.Walker <walkem47@my.erau.edu>
2019-08-07 08:51:22 -0700

    added  an if else statement

--- SpaceTradeGame/Game.cs
+++ SpaceTradeGame/Game.cs
@@ -16,7 +16,7 @@ namespace SpaceTradeGame
         public void gameIntro()
         {
             Console.WriteLine($"Welcome Space Trading, {character.name} ");
-            Console.WriteLine($"You are an {character.age} year old with {character.currency} Atoms in your account.");
+            Console.WriteLine($"You are an {character.age} year old.");
             Console.WriteLine("Press 'Enter' to begin game. ");
             Console.ReadLine();
             Console.Clear();
@@ -25,11 +25,11 @@ namespace SpaceTradeGame
 
         public void first()
         {
-            Console.WriteLine($"Your journey begins on your ship, on");
+            Console.WriteLine("Your journey begins on your ship, on Earth ");
             Console.WriteLine(" What do you want to do next?");
             Console.WriteLine(" 1. Go to your factory?");
             Console.WriteLine(" 2. View your account? ");
-            Console.WriteLine(" 3. visit another planet? ");
+            Console.WriteLine(" 3. Visit another planet? ");
             Console.Write("Choice: ");
             var choice = Console.ReadLine().ToLower();
             Console.Clear();
@@ -37,35 +37,78 @@ namespace SpaceTradeGame
             switch (choice)
             {
                 case "1":
-                    Console.WriteLine(" Welcome back to your factory.");
+                    Console.WriteLine(" Welcome back to your ship factory.");
+                    Console.ReadLine();
+                    second(); 
                     break; 
                 case "2":
-                    Console.WriteLine($" You now have {character.currency} Atoms");
-                    Console.WriteLine(" Earn more Atoms as you travel the universe and battle or trade your goods."); 
+                    Console.WriteLine($" You have {character.currency} Atoms in your account.");
+                    Console.WriteLine(" Earn more Atoms as you travel the universe and battle or trade your goods.");
+                    second();
                     break; 
                 case "3":
-                    Console.WriteLine("Which planet would you like to visit?"); 
+                    Console.WriteLine("Which planet would you like to visit?");
+                    Console.Write("Choice:  ");
+                    _ = Console.ReadLine().ToLower();
+                    Console.Clear();
+                    second(); 
                     break; 
-                    //Come up with statements that explain the outcome of your choice. 
-                    {
-                        
-                        Console.WriteLine("Press 'Enter' to continue");
-                        Console.ReadLine();
-                        gameOver();
-                        break;
-
-                    }
-
-
-
 
             }
         }
-        private  void gameOver()
+
+        public static void second()
+        {
+            Random rnd = new Random();
+            string[] secOptions = { "You notice a bomb, do you cut the red wire?", "You notice that you are overproduction, you " +
+                    "travel to other planets to sell your ships" ,"dfgdasdgdsg"};
+            
+            int randomNumber = rnd.Next(0, 3);
+            string secText = secOptions[randomNumber];
+
+            string secChoice;
+
+            Console.WriteLine(secText);
+            Console.WriteLine("Yes or No?");
+            Console.WriteLine("Choice: ");
+            secChoice = Console.ReadLine().ToLower();
+
+            if (secChoice == "yes" || secChoice == "yes")
+            {
+                third(); 
+
+            }
+            else if (secChoice == "no" || secChoice == "n")
+            {
+                Console.WriteLine(" An asteroid slams into planet earth and destroys everything.");
+                Console.WriteLine("Press 'Enter' to continue");
+                Console.ReadLine();
+               
+
+            }
+            else
+            {
+                Console.WriteLine(" ");
+            }
+
+            
+                
+          
+
+           
+        }
+        static void third()
+        { }
+
+
+
+        private void gameOver()
         {
             throw new NotImplementedException();
         }
 
 
     }
+
+   
 }
--- SpaceTradeGame/Planet.cs
+++ SpaceTradeGame/Planet.cs
@@ -1,4 +1,5 @@
-﻿using MediaFoundation.OPM;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using MediaFoundation.OPM;
 using System;
 using System.Drawing;
 
@@ -8,21 +9,28 @@ namespace SpaceTradeGame
 {
     public class Planet
     {
-        public string name { get; }
+        public string name;
+
         public Island[] Islands { get; set; }
 
-        public (double, double) location { get; set; }
+        public (double, double) location;
 
+        public Planet()
+        {
+            this.name = "Earth";
+            this.location = (10, 10);
+        }
 
+        //public Palanet()
+        // {
+        //   Object planet1 = new object; 
 
-
-        
-
-        
-       
     }
 
 }
+
+        
+
 
 
 
